ccde6e1f0462b9db68e9520299feb691917823d4
Sagar Rathod <97072541+sagarbrathod1@users.noreply.github.com>
2024-03-24 14:43:34 -0400

    delete contacts by id

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"html/template"
 	"io"
+	"strconv"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -26,17 +28,22 @@ type Count struct {
 	Count int
 }
 
+var id = 0
+
 type Contact struct {
 	Name  string
 	Email string
+	Id    int
 }
 
 type Contacts []Contact
 
 func newContact(name, email string) Contact {
+	id++
 	return Contact{
 		Name:  name,
 		Email: email,
+		Id:    id,
 	}
 }
 
@@ -51,6 +58,15 @@ func (d *Data) hasEmail(email string) bool {
 		}
 	}
 	return false
+}
+
+func (d *Data) indexOf(id int) int {
+	for i, contact := range d.Contacts {
+		if contact.Id == id {
+			return i
+		}
+	}
+	return -1
 }
 
 func newData() Data {
@@ -93,6 +109,9 @@ func main() {
 	page := newPage()
 	e.Renderer = newTemplate()
 
+	e.Static("/images", "images")
+	e.Static("/css", "css")
+
 	e.GET("/", func(c echo.Context) error {
 		return c.Render(200, "index", page)
 	})
@@ -115,6 +134,25 @@ func main() {
 
 		c.Render(200, "form", newFormData())
 		return c.Render(200, "oob-contact", contact)
+	})
+
+	e.DELETE("/contacts/:id", func(c echo.Context) error {
+		time.Sleep(3 * time.Second)
+
+		idStr := c.Param("id")
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			return c.String(400, "Invalid id")
+		}
+
+		index := page.Data.indexOf(id)
+		if index == -1 {
+			return c.String(404, "Contact not found")
+		}
+
+		page.Data.Contacts = append(page.Data.Contacts[:index], page.Data.Contacts[index+1:]...)
+
+		return c.NoContent(200)
 	})
 
 	e.Logger.Fatal(e.Start(":5000"))
